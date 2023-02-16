b8b3e97402ff639a5e5d0d80a9aadc4e64de649c
asiawoj1 <asiawoj1@gmail.com>
2023-02-16 20:38:09 +0100

    now its working

---
+++ src/main/java/org/example/Currency.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Currency {
+    String code = "";
+    URL quarry = null;
+    Double amount = 0.0;
+    Double mid = 1.0;
+
+
+    public Currency() {}
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public void setQuarry(String quarry) throws MalformedURLException {
+        this.quarry = new URL(quarry);;
+    }
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+    public void setMid(Double mid) {
+        this.mid = mid;
+    }
+    public String getCode() {
+        return this.code;
+    }
+    public URL getQuarry() {
+        return this.quarry;
+    }
+    public Double getAmount() {
+        return this.amount;
+    }
+    public Double getMid() {
+        return this.mid;
+    }
+}
